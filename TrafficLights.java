0e7b943f0c7c689290f7957c58ed5c3cb194c494
RieraDiPe <rieradipe@gmail.com>
2025-02-21 16:39:19 +0100

    Quedan dos

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,15 +1,35 @@
 public class TrafficLights {
     public static void main(String[] args) {
 
-        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
+        /*Escribe un programa que basado en el color del semáforo verde, amarillo 
+        o rojo determine las acciones de un conductor*/
+        String color = "azul"; 
         
         //Si es verde imprime en la terminal: Puedes seguir
+        if (color == "verde") {
+            System.out.println("Puedes pasar");
+        } else if 
+            (color == "amarillo") {
+                System.out.println("Ten cuidado");
+            } else if 
+            (color.equals("rojo")) {
+                System.out.println("Paraaa");
+            } else {
+            System.out.println("El semaforo esta roto");
+            }
+        };
+    }
+        
+        
+        
+          
+
         //Si es amarillo imprime en la terminal: Ten cuidado
         //Si es rojo imprime en la terminal: ¡¡¡Para!!!
         //Si es otro color imprime en la terminal: El semáforo está dañado
 
         //Cambia el valor de la variable, corre el programa y mira el resultado 
 
-    }
     
-}
+    
+
